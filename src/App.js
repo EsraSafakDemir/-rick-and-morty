b035a1521afb1d11824d522e773ec4833fd45e06
EsraSafakDemir <esraasdemir@gmail.com>
2021-02-10 23:47:18 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react'
-import Search from './components/Search'
-import CharacterCard from './components/CharacterCard/CharacterCard'
-import {BrowserRouter , Router, Route} from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
 
-import {Link} from 'react-router-dom'
 
 
-
-function App() {
-  const apiurl ="http://www.omdbapi.com/?i=tt3896198&apikey=873b707";
- 
+export default function App() {
   return (
-    <div className="App">
-      <header >
-        <h1>Rick and Morty</h1>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Character Details</Link>
+          </li>
+           <li>
+            <Link to="/characterlist">Character List</Link>
+          </li>
+        </ul>
 
-      <main>
-        <Search/>
-      </main>
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <CharacterDetail />
+          </Route>
+          <Route path="/characterlist">
+            <CharacterList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function CharacterDetail() {
+  return (
+    <div>
+      <h2>Characters Detail</h2>
     </div>
   );
 }
 
-export default App
+function CharacterList() {
+  
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>List of characters</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/Rick`}>Rick</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Morty`}>Morty</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Rick and Morty`}>Rick and Morty</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+ 
+  let { topicId } = useParams();
+
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, DatePicker, version } from "antd";
-
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
-import { message, Alert } from 'antd';
+import { DatePicker, message, Alert } from 'antd';
 import 'antd/dist/antd.css';
 
 
 ReactDOM.render(
-  <div className="App">
-    <h1>antd version: {version}</h1>
-    <DatePicker />
-    <Button type="primary" style={{ marginLeft: 8 }}>
-      Primary Button
-    </Button>
-  </div>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 
 
